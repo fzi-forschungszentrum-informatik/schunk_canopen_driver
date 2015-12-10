6d8e358316ebceb599593f6f8af6ba2f7cb1e2e8
Felix Mauch <mauch@fzi.de>
2015-12-10 21:51:34 +0100

    added more documentation to services and launch files

--- include/schunk_canopen_driver/SchunkCanopenNode.h
+++ include/schunk_canopen_driver/SchunkCanopenNode.h
@@ -48,23 +48,82 @@ private:
   // Action interfaces for standalone mode (without ros_control)
   void goalCB(actionlib::ServerGoalHandle<control_msgs::FollowJointTrajectoryAction> gh);
   void cancelCB(actionlib::ServerGoalHandle<control_msgs::FollowJointTrajectoryAction> gh);
+  /*!
+   * \brief Control loop thread when not using ros_control
+   */
   void trajThread(actionlib::ServerGoalHandle< control_msgs::FollowJointTrajectoryAction >& gh);
 
-  // Service callback handlers
+
+  /*!
+   * \brief This service call enables the devices after a fault, a quick stop or simply
+   * when the brakes have been closed by hand (via the close_brakes service call)
+   */
   bool enableNodes(std_srvs::TriggerRequest& req, std_srvs::TriggerResponse& resp);
+
+  /*!
+   * \brief Performs a quick stop on all nodes. You should prefer this service call
+   * to aborting the followJointTrajectory action, as this will instantaniously stop
+   * the robot's movement without resulting in a fault state.
+   */
   bool quickStopNodes(std_srvs::TriggerRequest& req, std_srvs::TriggerResponse& resp);
+
+  /*!
+   * \brief Perform a reset offset for a given list of nodes. You should call this
+   * service, after driving the device to it's zero position manually. Only the nodes
+   * given in the node list are affected by this service call. The position of all unlisted
+   * joints is irrelevant.
+   *
+   * \note: This service call exists for canopen IDs (usually 3-8 for a lwa4p) and
+   * as an interface using the joint names from the URDF.
+   */
   bool homeNodesCanIds(schunk_canopen_driver::HomeWithIDsRequest& req,
                        schunk_canopen_driver::HomeWithIDsResponse& resp);
+
+  /*!
+   * \brief Perform a reset offset for a given list of nodes. You should call this
+   * service, after driving the device to it's zero position manually. Only the nodes
+   * given in the node list are affected by this service call. The position of all unlisted
+   * joints is irrelevant.
+   *
+   * \note: This service call exists for canopen IDs (usually 3-8 for a lwa4p) and
+   * as an interface using the joint names from the URDF.
+   */
   bool homeNodesJointNames(schunk_canopen_driver::HomeWithJointNamesRequest& req,
                            schunk_canopen_driver::HomeWithJointNamesResponse& resp);
+
+  /*!
+   * \brief Perform a reset offset for all nodes. You should call this
+   * service, after driving the devices to their zero position manually.
+   */
   bool homeAllNodes(schunk_canopen_driver::HomeAllRequest& req,
                     schunk_canopen_driver::HomeAllResponse& resp);
+
+
+  /*!
+   * \brief Close the brakes on all nodes. Use this when the device is standing still
+   * for a longer time to reduce hight motor currents for doing nothing.
+   *
+   * Remember to call the enable_nodes service when you want to go on.
+   */
   bool closeBrakes (std_srvs::TriggerRequest& req, std_srvs::TriggerResponse& resp);
 
+  /*!
+   * \brief Triggers initialization of the canopen devices.
+   *
+   * This service will only be advertised when the autostart parameter is set to
+   * false. Otherwise initialization will be triggered automatically at startup.
+   */
   bool initDevicesCb (std_srvs::TriggerRequest& req, std_srvs::TriggerResponse& resp);
 
+  /*!
+   * \brief Function that actually initializes the devices. Will get called either on
+   * startup (if autostart is allowed) or by the init_devices service call.
+   */
   void initDevices();
 
+  /*!
+   * \brief When using ros_control this will perform the control loop in a separate thread.
+   */
   void rosControlLoop ();
 
 
