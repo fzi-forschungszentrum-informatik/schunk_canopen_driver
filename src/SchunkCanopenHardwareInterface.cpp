55354f6104642f758c8256b015c8b915f2b475fa
Georg Heppner <heppner@fzi.de>
2016-04-04 14:25:25 +0200

    Added Licence Texts in files

--- src/SchunkCanopenHardwareInterface.cpp
+++ src/SchunkCanopenHardwareInterface.cpp
@@ -1,6 +1,15 @@
 // this is for emacs file handling -*- mode: c++; indent-tabs-mode: nil -*-
 
 // -- BEGIN LICENSE BLOCK ----------------------------------------------
+// This file is part of the SCHUNK Canopen Driver suite.
+//
+// This program is free software licensed under the LGPL
+// (GNU LESSER GENERAL PUBLIC LICENSE Version 3).
+// You can find a copy of this license in LICENSE folder in the top
+// directory of the source code.
+//
+// © Copyright 2016 SCHUNK GmbH, Lauffen/Neckar Germany
+// © Copyright 2016 FZI Forschungszentrum Informatik, Karlsruhe, Germany
 // -- END LICENSE BLOCK ------------------------------------------------
 
 //----------------------------------------------------------------------
